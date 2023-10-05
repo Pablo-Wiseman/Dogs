feae2d70bde2b38c43ad9b823477d6851c1fa2cb
Pablo-Wiseman <accessdevelopment@yandex.ru>
2023-10-05 19:39:46 +0500

    Internet access & stringbilder

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.dogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadDogImage();
+    }
+
+
+    private void loadDogImage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                    InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder data = new StringBuilder();
+                    String result;
+                    do {
+                       result =  bufferedReader.readLine();
+                       if (result !=null) {
+                           data.append(result);
+                       }
+                    } while (result != null);
+
+                    Log.d("mainActivity", data.toString());
+                } catch (Exception e) {
+                    Log.d("MainActivity", e.toString());
+                }
+            }
+        }).start();
+
     }
 }
